ffb04b66570b1b1da1bcadb187e4183dfbcb626f
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-06-12 20:28:36 +0100

    firebase env

--- server/firebase.js
+++ server/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey: 'AIzaSyAKZfNe6FZHUBlJmtscQE6EbSj9Xx9FK3E',
+    authDomain: 'julianamoraiswebapp.firebaseapp.com',
+    databaseURL: 'https://julianamoraiswebapp.firebaseio.com',
+    projectId: 'julianamoraiswebapp',
+    storageBucket: 'julianamoraiswebapp.appspot.com',
+    messagingSenderId: '418768078635',
+    appId: '1:418768078635:web:f833943fbbe53481a0676e',
+    measurementId: 'G-3WBPJVHNJJ'
 }
 
 !firebase.apps.length ? firebase.initializeApp(config) : '';
